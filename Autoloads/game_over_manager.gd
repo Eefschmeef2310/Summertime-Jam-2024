2072ad8ab46a87293d08f321cb5c48b66728fec6
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-01-25 10:02:36 +1100

    Game Over update

--- Autoloads/game_over_manager.gd
+++ Autoloads/game_over_manager.gd
@@ -2,5 +2,7 @@ extends Node
 
 var game_over_screen : PackedScene = preload("res://Levels/menus/game_over_screen.tscn")
 
-func game_over():
-	get_tree().root.add_child(game_over_screen.instantiate())
+func game_over(reason : String):
+	var game_over = game_over_screen.instantiate()
+	game_over.reason = reason
+	get_tree().root.add_child(game_over)
